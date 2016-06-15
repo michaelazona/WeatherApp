4372c2a900f4b021a2821f23e5de588b9d5e012a
mint <michaelazona@gmail.com>
2016-06-15 15:10:34 -0500

    Added all JS Unit tests!

--- js/jsFunctions.js
+++ js/jsFunctions.js
@@ -27,6 +27,7 @@ function timeStampConverter(timestamp)
 
 function capitalizeWords(phrase)
 {
+	phrase = phrase.toLowerCase()
 	phrase = phrase.split(" ");
 
 	for(var i = 0; i < phrase.length; i++)
@@ -34,6 +35,59 @@ function capitalizeWords(phrase)
 		phrase[i] = phrase[i].charAt(0).toUpperCase() + phrase[i].slice(1);
 	}
 
-	phrase = phrase.join().replace(",", " ");
+	phrase = phrase.join().replace(new RegExp(',', 'g'), ' ');
 	return phrase;
 }
+
+function removeCommas(phrase)
+{
+	return phrase.replace(new RegExp(',', 'g'), '');
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//UNIT TESTS - USING MOCHA//
+
+var assert = require('chai').assert;
+
+describe('validateZip', function() {
+    it('it should return true if the zip code is valid', function () {
+      assert.equal(true, validateZip("55431"));
+      assert.equal(false, validateZip("554319"));
+    });
+});
+
+describe('validateEmail', function() {
+    it('it should return true if the email is valid', function () {
+      assert.equal(true, validateEmail("email@sample.com"));
+      assert.equal(false, validateEmail("email.sample@com"));
+      assert.equal(false, validateEmail("email.com"));
+      assert.equal(false, validateEmail("email@sample"));
+      assert.equal(false, validateEmail("gibberish"));
+    });
+});
+
+describe('timeStampConverter', function() {
+    it('it should return a valid formatted time when given a unix time stamp', function () {
+      assert.equal("5:25", timeStampConverter("1465986347"));
+      assert.equal("9:02", timeStampConverter("1466042522"));
+      assert.notEqual("5:24", timeStampConverter("1465986347"));
+      assert.notEqual("9:03", timeStampConverter("1466042522"));
+    });
+});
+
+describe('capitalizeWords', function() {
+    it('it should take any phrase and capitalize the first letter of words, and make all others lower case', function () {
+      assert.equal("Hello World", capitalizeWords("hello world"));
+      assert.equal("Hello World", capitalizeWords("hELLO WORLD"));
+      assert.equal("Hello World How Are You?", capitalizeWords("hELLO wORLD how ARE YoU?"));
+    });
+});
+
+describe('removeCommas', function() {
+    it('it should take any string and remove the comma', function () {
+      assert.equal("Hello World", removeCommas("H,e,l,l,o W,o,r,l,d"));
+      assert.equal("Hello World", removeCommas("H,,ell,,o ,Wo,rl,d,,"));
+    });
+});
