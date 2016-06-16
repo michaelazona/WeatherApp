7bc9becaca5d4d42c137443facc9ecf2ff3b06ed
mint <michaelazona@gmail.com>
2016-06-16 08:00:16 -0500

    Organzing some PHP functions

--- Weather/phpFunctions/functions.php
+++ Weather/phpFunctions/functions.php
@@ -24,10 +24,28 @@
 		{
 			$sql = "DELETE FROM `emails` WHERE `email`='${email}'";
 			$conn->query($sql);
-			$sql = "INSERT INTO `emails` (`email`, `lastCity`, `lastZip`) VALUES ('${email}', '${city}', '${zip}')";
+			$sql = assembleSQLForAddingEmail($email, $city, $zip);
 			$conn->query($sql);
 		}
 	}
 
-	
+	function assembleSQLForAddingEmail($email, $city, $zip)
+	{
+		$sql = "INSERT INTO `emails` (`email`, `lastCity`, `lastZip`) VALUES ('${email}', '${city}', '${zip}')";
+		return $sql;
+	}
+
+	function validateZipWithCity($city, $zip)
+	{
+		$city = strToLower($city);
+		$returnValue = "FALSE";
+		$data = strtolower(file_get_contents("http://maps.googleapis.com/maps/api/geocode/json?address=${zip}&sensor=true"));
+		
+		if (strpos($data, $city) !== false) 
+		{
+		    $returnValue = "TRUE";
+		}
+		
+		return $returnValue;
+	}
 ?>
--- Weather/phpFunctions/validateZip.php
+++ Weather/phpFunctions/validateZip.php
@@ -1,17 +1,10 @@
 <?php
 
+	include 'functions.php';
+
 	$city = $_GET['city'];
 	$zip  = $_GET['zip'];
 
-	$city = strToLower($city);
-	$returnValue = "FALSE";
-	$data = strtolower(file_get_contents("http://maps.googleapis.com/maps/api/geocode/json?address=${zip}&sensor=true"));
-	
-	if (strpos($data, $city) !== false) 
-	{
-	    $returnValue = "TRUE";
-	}
-	
-	echo $returnValue;
+	echo validateZipWithCity($city, $zip);
 	
 ?>
